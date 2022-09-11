821b9ff9519f5219ffaf6364dd644f0ce796541e
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-11 14:33:57 -0300

    Criando Classes e objetos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,32 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val contaMatheus = Conta()
+    contaMatheus.titular = "Alex"
+    contaMatheus.numero = 1000
+    contaMatheus.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaMatheus.titular)
+    println(contaMatheus.numero)
+    println(contaMatheus.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaLacos() {
     for (i in 1..5) {
         val titular: String = "Matheus $i"
         val numeroConta: Int = 1000 + i
