1f9b2cb0bed1b506b8295f42d145b62b3ffad313
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-11 12:00:03 -0300

    Declarando os tipos das variáveis e printado-as

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Matheus"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100.0
+
+    println("titular $titular") // titular Matheus
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+}
