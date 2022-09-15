4725b2428e90ef6be0afde8feed7e0dc22e3dbc8
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-14 22:46:44 -0300

    Herança - Criando as classes de Gerente e Diretor

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,37 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val matheus = Funcionario(
-        nome = "Matheus",
-        cpf = "111.111.111-11",
-        salario = 1000.0
-    )
+    val matheus = Funcionario(nome = "Matheus", cpf = "111.111.111-11", salario = 1000.0)
 
     println("nome ${matheus.nome}")
     println("cpf ${matheus.cpf}")
     println("salario ${matheus.salario}")
     println("bonificação ${matheus.bonificacao()}")
+
+    val fran = Gerente(nome = "Fran", cpf = "222.222.222-22", salario = 2000.0, senha = 123)
+
+    println("nome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("bonificação ${fran.bonificacao()}")
+
+    if (fran.autenticacao(senha = 123)) {
+        println("Autenticado com Sucesso!")
+    } else {
+        println("Falha na Autenticação")
+    }
+
+    val gui = Diretor(nome = "Gui", cpf = "333.333.333-33", salario = 4000.0, senha = 4000, plr = 200.0)
+
+    println("nome ${gui.nome}")
+    println("cpf ${gui.cpf}")
+    println("salario ${gui.salario}")
+    println("bonificação ${gui.bonificacao()}")
+    println("plr ${gui.plr}")
+
+    if (gui.autenticacao(senha = 123)) {
+        println("Autenticado com Sucesso!")
+    } else {
+        println("Falha na Autenticação")
+    }
 }
