2a0f3c0d14e3aaef9d792d34fe79b47f7a678627
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-22 09:10:29 -0300

    Exception: Lhe dando com exeções ...

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,24 @@
+import MatheusADSantos.com.github.bytebank.modelo.Endereco
 import MatheusADSantos.com.github.bytebank.teste.testaAny
 
 fun main() {
-    testaAny()
+    println("início main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5) {
+        println(i)
+        var endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
 }
