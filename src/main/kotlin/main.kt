d24eb02ecaa13edb51f256c704bc0ace97fbe011
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-22 10:11:42 -0300

    Try-Expression: Try Catch pra pegar excessões como ClassCastException

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 import MatheusADSantos.com.github.bytebank.modelo.Endereco
 import MatheusADSantos.com.github.bytebank.teste.testaAny
+import java.lang.ClassCastException
 
 fun main() {
     println("início main")
@@ -9,7 +10,11 @@ fun main() {
 
 fun funcao1(){
     println("início funcao1")
-    funcao2()
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        println("ClassCastException pega!!!")
+    }
     println("fim funcao1")
 }
 
