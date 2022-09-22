8c610b739c9190775708f6a627aba3d70bac60d4
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-22 11:42:11 -0300

    Try/If-Expressions: Tratando e já jogando a info tratada na variavel

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,30 @@
 import MatheusADSantos.com.github.bytebank.modelo.Endereco
-import MatheusADSantos.com.github.bytebank.teste.testaAny
-import java.lang.ClassCastException
 
 fun main() {
     println("início main")
+    val entrada: String = "1.0"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("Valor Recebido: $valorComTaxa")
+    } else {
+        println("Valor Inválido")
+    }
+
+
     funcao1()
     println("fim main")
 }
@@ -14,6 +35,8 @@ fun funcao1(){
         funcao2()
     } catch (e: ClassCastException) {
         println("ClassCastException pega!!!")
+        println(e.message) // Retorna a mensagem do erro
+        println(e.printStackTrace()) // Ao final do código ele retorna o tipo/mensagem rastreio da pilha ...
     }
     println("fim funcao1")
 }
