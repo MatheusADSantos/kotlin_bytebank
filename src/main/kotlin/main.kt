f99f3f5e9f496dcaeb305f3e4a27ddf51f6b223b
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-26 12:15:58 -0300

    Safe Cast: Retornando null quando vier tipo diferente do esperado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,24 @@
 import MatheusADSantos.com.github.bytebank.modelo.Endereco
+import kotlin.reflect.typeOf
 
 fun main() {
 
     // Safe Call - Com -> '?.let' -> Análoga ao 'if let' do swift
-    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Francisco Paranhos")
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Francisco Paranhos", complemento = "prédio")
     val logradouroNovo: String? = enderecoNulo?.logradouro
     enderecoNulo?.let {
         println(it.logradouro.length)
+        // Testando o Elvis Operator
+        val tamanhoComplemento: Int = it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+        println(tamanhoComplemento)
     }
 
-    // Chamadas sem o let
-    enderecoNulo?.logradouro?.length
+    teste("")
+    teste(1)
+}
 
-
+// Testando o Safe Cast - Caso venha algo diferente de um número, retorna null
+fun teste(valor: Any) {
+    val numero: Int? = valor as? Int
+    println(numero)
 }
