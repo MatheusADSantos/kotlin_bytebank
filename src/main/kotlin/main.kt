1c4f9ead6244e8f1c3b2f65d9b9f7309a6fa8181
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-30 21:41:18 -0300

    Iniciando uma val do tipo função

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,22 @@
-import MatheusADSantos.com.github.bytebank.modelo.Endereco
-import kotlin.reflect.typeOf
+import MatheusADSantos.com.github.bytebank.teste.testaExpressao
 
 fun main() {
 
-    // Safe Call - Com -> '?.let' -> Análoga ao 'if let' do swift
-    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Francisco Paranhos", complemento = "prédio")
-    val logradouroNovo: String? = enderecoNulo?.logradouro
-    enderecoNulo?.let {
-        println(it.logradouro.length)
-        // Testando o Elvis Operator
-        val tamanhoComplemento: Int = it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
-        println(tamanhoComplemento)
-    }
+    val minhaFuncao = ::teste
+    println(minhaFuncao())
 
-    teste("")
-    teste(1)
+    val minhaFuncaoClasse = ::Teste
+    println(minhaFuncaoClasse())
+
 }
 
-// Testando o Safe Cast - Caso venha algo diferente de um número, retorna null
-fun teste(valor: Any) {
-    val numero: Int? = valor as? Int
-    println(numero)
+fun teste() {
+    println("Executa teste")
+}
+
+class Teste: () -> Unit {
+    override fun invoke() {
+        println("Executa invoke do Teste")
+    }
+
 }
