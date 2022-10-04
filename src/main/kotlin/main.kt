ab955937104bf92f28caa8cc2f88b7a91addb31a
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-04 09:03:53 -0300

    Trabalhando com função ANONIMA e LAMBDA 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,17 +5,35 @@ fun main() {
 //    testaTipoFuncaoReferencia()
 //    testaTipoFuncaoClasse()
 
-    val minhaFuncaoLambda = { a: Int, b: Int -> Int
-        println("Executa como LAMBDA")
-        a + b
-    }
-    println(minhaFuncaoLambda(20, 15))
+//    val minhaFuncaoLambda = { a: Int, b: Int -> Int
+//        println("Executa como LAMBDA")
+//        a + b
+//    }
+//    println(minhaFuncaoLambda(20, 15))
 
-    val minhaFuncaoAnonima= fun(a: Int, b: Int): Int {
-        println("Executa como ANONIMA")
-        return a + b
+//    val minhaFuncaoAnonima= fun(a: Int, b: Int): Int {
+//        println("Executa como ANONIMA")
+//        return a + b
+//    }
+//    println(minhaFuncaoAnonima(10, 20))
+
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{salario ->
+        if (salario > 1000.0) {
+            return@lambda salario + 50
+        } else {
+            salario+ 100.0
+        }
     }
-    println(minhaFuncaoAnonima(10, 20))
+    println(calculaBonificacao(1500.0))
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if(salario > 1000.0){
+            return salario + 50
+        }
+        return salario + 100.0
+    }
+    println(calculaBonificacaoAnonima(1100.0))
+
 
 }
 
