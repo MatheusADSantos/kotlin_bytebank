5074ecd621689516924fa626c4b4526e9edb2a5d
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-10 09:51:53 -0300

    run: Testando ...

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,33 @@
-import MatheusADSantos.com.github.bytebank.modelo.Endereco
+import MatheusADSantos.com.github.bytebank.modelo.*
 
 fun main() {
+//    testaWith()
+
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    val contaPoupanca = ContaPoupanca(Cliente(nome = "Matheus Augusto",cpf = "111.111.111-11", senha = 1234), 1000)
+
+    contaPoupanca.run {
+        deposita(1000.0)
+        saldo * taxaMensal
+    }.let { rendimentoMensal ->
+        println("Rendimento Mensal: $rendimentoMensal")
+    }
+
+    val rendimentoAnual = run {
+        var saldo = contaPoupanca.saldo
+        repeat(12) {
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+    println("Simulação do Rendimento Anual: $rendimentoAnual")
+
+
+
+}
+
+fun testaWith() {
     with(Endereco()) {
         logradouro = "Rua Francisco Paranhos"
         numero = 318
